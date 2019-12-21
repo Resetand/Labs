a84a7b4c57c030574eb3089f74cbc93b596055eb
Vlad Didenko <vladislav.didenko.20001@yandex.ru>
2019-12-21 18:20:47 +0300

    update reqs

--- lab_1.py
+++ lab_1.py
@@ -2,6 +2,9 @@ import cv2
 from lib.utils import Utils
 from lib.helpers import ImageURL
 
-cv2.imshow('GRAYSCALE image', Utils.fetch_image(
-    url=ImageURL.RANDOM, flag=cv2.IMREAD_GRAYSCALE))
+img =  Utils.fetch_image(url=ImageURL.RANDOM)
+print(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+Utils.show_image_compare(img, gray)
 cv2.waitKey(0)
